64b21148a3e42bf8c0f60df00eba39aa7ab77c86
Jahn16 <jpdeoliveiraferreira@gmail.com>
2025-04-03 20:10:03 -0300

    feat: add songs and play button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { Howl } from 'howler';
+
+	import songs from '$lib/data/songs.json';
+
+	let sound1 = new Howl({ src: songs[0].url });
+	let sound2 = new Howl({ src: songs[1].url });
+
+	sound1.once('play', async () => {
+		await new Promise((resolve) => setTimeout(resolve, 10000));
+		sound1.stop();
+		sound2.stop();
+	});
+	const play = () => {
+		sound1.play();
+	};
+	const changeTrack = () => {
+		sound1.stop();
+		sound2.play();
+	};
+</script>
+
+<button on:click={play}>Play</button>
+
+<button on:click={changeTrack}>Troca</button>
