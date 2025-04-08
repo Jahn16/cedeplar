74a7804b71761a3bac05b5965a4277ff4f203e77
Jahn16 <jpdeoliveiraferreira@gmail.com>
2025-04-07 22:28:23 -0300

    fix: disable button after swap

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -51,7 +51,7 @@
 						Play
 					</button>
 				{:else}
-					<button class="btn" on:click={changeTrack}>
+					<button class="btn" on:click={changeTrack} disabled={currentSoundIndex != 0}>
 						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
 							><path fill="currentColor" d="m21 9l-4-4v3h-7v2h7v3M7 11l-4 4l4 4v-3h7v-2H7z" /></svg
 						>
