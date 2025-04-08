25e6cd1df447133e112bdee7bdc3093818c315d2
Jahn16 <jpdeoliveiraferreira@gmail.com>
2025-04-07 22:23:29 -0300

    feat: add card

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,15 +8,17 @@
 	let sound1 = new Howl({ src: songs[0].url });
 	let sound2 = new Howl({ src: songs[1].url });
 	let isPlaying = false;
-	let counter = 10;
+	let counter = 0;
+	const maxValue = 15;
+	let currentSoundIndex = 0;
 
 	sound1.once('play', () => {
 		let intervalID = setInterval(() => {
-			if (counter == 0) {
+			if (counter == maxValue) {
 				stop();
 				clearInterval(intervalID);
 			}
-			counter--;
+			counter++;
 		}, 1000);
 	});
 	const stop = () => {
@@ -29,26 +31,34 @@
 		sound1.play();
 	};
 	const changeTrack = () => {
+		currentSoundIndex++;
 		sound1.stop();
 		sound2.play();
 	};
 </script>
 
-{#if !isPlaying}
-	<button class="btn" on:click={play}>
-		<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-			><path fill="currentColor" d="M8.5 8.64L13.77 12L8.5 15.36zM6.5 5v14l11-7" /></svg
-		>
-		Play
-	</button>
-{:else}
-	<button class="btn" on:click={changeTrack}>
-		<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-			><path fill="currentColor" d="m21 9l-4-4v3h-7v2h7v3M7 11l-4 4l4 4v-3h7v-2H7z" /></svg
-		>
-		Trocar
-	</button>
-{/if}
-<span class="countdown">
-	<span style="--value:{counter};" aria-live="polite">{counter}</span>
-</span>
+<div class="flex justify-center">
+	<div class="card w-96 bg-base-100 card-md shadow-sm">
+		<div class="card-body">
+			<h2 class="card-title">{songs[currentSoundIndex].name}</h2>
+			<progress class="progress w-80" value={counter} max="15"></progress>
+			<div class="card-actions justify-end">
+				{#if !isPlaying}
+					<button class="btn" on:click={play}>
+						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+							><path fill="currentColor" d="M8.5 8.64L13.77 12L8.5 15.36zM6.5 5v14l11-7" /></svg
+						>
+						Play
+					</button>
+				{:else}
+					<button class="btn" on:click={changeTrack}>
+						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+							><path fill="currentColor" d="m21 9l-4-4v3h-7v2h7v3M7 11l-4 4l4 4v-3h7v-2H7z" /></svg
+						>
+						Trocar
+					</button>
+				{/if}
+			</div>
+		</div>
+	</div>
+</div>
