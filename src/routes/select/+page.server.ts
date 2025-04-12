114a5010615e7a8b852d2bad45c7532448dc127b
Jahn16 <jpdeoliveiraferreira@gmail.com>
2025-04-12 17:10:14 -0300

    feat: redirect in form action

--- src/routes/select/+page.server.ts
+++ src/routes/select/+page.server.ts
@@ -1,7 +1,14 @@
-import type { PageLoad } from './$types';
+import type { PageLoad, Actions } from './$types';
+import { redirect } from '@sveltejs/kit';
 import songs from '$lib/data/songs.json';
 export const load: PageLoad = () => {
 	return {
 		genres: [...new Set(songs.map((obj) => obj.genre))]
 	};
 };
+
+export const actions = {
+	default: async () => {
+		redirect(303, '/');
+	}
+} satisfies Actions;
