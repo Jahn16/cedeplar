99bae70b0f10c4d9489bcdb9cfa5c04144d00335
Jahn16 <jpdeoliveiraferreira@gmail.com>
2025-04-12 10:57:51 -0300

    fix: format

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -37,28 +37,26 @@
 	};
 </script>
 
-<div class="flex justify-center">
-	<div class="card w-96 bg-base-100 card-md shadow-sm">
-		<div class="card-body">
-			<h2 class="card-title">{songs[currentSoundIndex].name}</h2>
-			<progress class="progress w-80" value={counter} max="15"></progress>
-			<div class="card-actions justify-end">
-				{#if !isPlaying}
-					<button class="btn" on:click={play}>
-						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-							><path fill="currentColor" d="M8.5 8.64L13.77 12L8.5 15.36zM6.5 5v14l11-7" /></svg
-						>
-						Play
-					</button>
-				{:else}
-					<button class="btn" on:click={changeTrack} disabled={currentSoundIndex != 0}>
-						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-							><path fill="currentColor" d="m21 9l-4-4v3h-7v2h7v3M7 11l-4 4l4 4v-3h7v-2H7z" /></svg
-						>
-						Trocar
-					</button>
-				{/if}
-			</div>
+<div class="card w-96 bg-base-100 card-md shadow-sm">
+	<div class="card-body">
+		<h2 class="card-title">{songs[currentSoundIndex].name}</h2>
+		<progress class="progress w-80" value={counter} max="15"></progress>
+		<div class="card-actions justify-end">
+			{#if !isPlaying}
+				<button class="btn" on:click={play}>
+					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+						><path fill="currentColor" d="M8.5 8.64L13.77 12L8.5 15.36zM6.5 5v14l11-7" /></svg
+					>
+					Play
+				</button>
+			{:else}
+				<button class="btn" on:click={changeTrack} disabled={currentSoundIndex != 0}>
+					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+						><path fill="currentColor" d="m21 9l-4-4v3h-7v2h7v3M7 11l-4 4l4 4v-3h7v-2H7z" /></svg
+					>
+					Trocar
+				</button>
+			{/if}
 		</div>
 	</div>
 </div>
