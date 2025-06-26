0dcaf3070c5c52b180b88061a5796bcf4f5f1dca
Jahn16 <jpdeoliveiraferreira@gmail.com>
2025-06-25 21:44:26 -0300

    feat: add questions and render into page

--- src/routes/survey/+page.svelte
+++ src/routes/survey/+page.svelte
@@ -1,68 +1,50 @@
 <script lang="ts">
+	import questionsData from '$lib/data/questions.json';
+
+	interface Question {
+		question: string;
+		type: string;
+		options?: string[];
+	}
+
+	const questions: Question[] = questionsData as Question[];
+
 	let step = 1;
+	let question = questions[step - 1];
+	let inputName = `question-${step}`;
+	let answer = '';
 </script>
 
 <div></div>
 <div class="card w-96 bg-base-100 card-lg shadow-sm">
 	<div class="card-body">
-		{#if step == 1}
-			<h2 class="card-title">
-				Com que frequência você usa o Spotify ou outro streaming de música para ouvir músicas?
-			</h2>
-			<label>
-				<input type="radio" name="freq-streaming" value="todos" class="radio" />
-				Todos os dias
-			</label><br />
-			<label>
-				<input type="radio" name="freq-streaming" value="5-6" class="radio" />
-				5 a 6 dias na semana
-			</label><br />
-			<label>
-				<input type="radio" name="freq-streaming" value="4-5" class="radio" />
-				4 a 5 dias na semana
-			</label><br />
-			<label>
-				<input type="radio" name="freq-streaming" value="3-4" class="radio" />
-				3 a 4 dias na semana
-			</label><br />
-			<label>
-				<input type="radio" name="freq-streaming" value="2-3" class="radio" />
-				2 a 3 dias na semana
-			</label><br />
-			<label>
-				<input type="radio" name="freq-streaming" value="1" class="radio" />
-				Uma vez na semana
-			</label><br />
-		{:else}
-			<h2 class="card-title">
-				Com que frequência você usa o Spotify ou outro streaming de música para descobrir uma música
-				nova ou um novo artista?
-			</h2>
-
-			<label>
-				<input type="radio" name="freq-new" value="sempre" class="radio" />
-				Sempre
-			</label><br />
-			<label>
-				<input type="radio" name="freq-new" value="quase-sempre" class="radio" />
-				Quase sempre
-			</label><br />
-			<label>
-				<input type="radio" name="freq-new" value="as-vezes" class="radio" />
-				Às vezes
-			</label><br />
-			<label>
-				<input type="radio" name="freq-new" value="quase-nunca" class="radio" />
-				Quase nunca
-			</label><br />
-			<label>
-				<input type="radio" name="freq-new" value="nunca" class="radio" />
-				Nunca
-			</label><br />
+		<h2 class="card-title">
+			{question.question}
+		</h2>
+		{#if question.options}
+			<!-- eslint-disable svelte/require-each-key -->
+			{#each question.options as option}
+				<label>
+					<input type="radio" name={inputName} value={option} class="radio" bind:group={answer} />
+					{option}
+				</label><br />
+			{/each}
+		{:else if question.type === 'text'}
+			<input class="input" id={inputName} bind:value={answer} />
 		{/if}
 
 		<div class="justify-end card-actions">
-			<button class="btn btn-primary" on:click={step++}>Proximo</button>
+			<button
+				class="btn btn-primary"
+				type="submit"
+				on:click={() => {
+					step++;
+					question = questions[step - 1];
+					inputName = `question-${step}`;
+					console.log(answer);
+					answer = '';
+				}}>Proximo</button
+			>
 		</div>
 	</div>
 </div>
