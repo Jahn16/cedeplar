daeec143e551678e35735fc65f49d9e199e6eab9
Jahn16 <jpdeoliveiraferreira@gmail.com>
2025-07-03 19:42:45 -0300

    feat: add radio table

--- src/routes/survey/+page.svelte
+++ src/routes/survey/+page.svelte
@@ -1,20 +1,20 @@
 <script lang="ts">
-	import questionsData from '$lib/data/questions.json';
-
-	interface Question {
-		question: string;
-		type: string;
-		options?: string[];
-	}
-
-	const questions: Question[] = questionsData as Question[];
+	import questions from '$lib/data/questions.json';
 
 	let step = 1;
 	let question = questions[step - 1];
 	let inputName = `question-${step}`;
-	let answer = '';
+
+	const getAnswers = () => {
+		document
+			.querySelectorAll('input[type="radio"]:checked, input[type="text"]')
+			.forEach((input) => {
+				console.log(`${input.value}`);
+			});
+	};
 </script>
 
+<!-- eslint-disable svelte/require-each-key -->
 <div></div>
 <div class="card w-96 bg-base-100 card-lg shadow-sm">
 	<div class="card-body">
@@ -22,15 +22,36 @@
 			{question.question}
 		</h2>
 		{#if question.options}
-			<!-- eslint-disable svelte/require-each-key -->
 			{#each question.options as option}
 				<label>
-					<input type="radio" name={inputName} value={option} class="radio" bind:group={answer} />
+					<input type="radio" name={inputName} value={option} class="radio" />
 					{option}
 				</label><br />
 			{/each}
 		{:else if question.type === 'text'}
-			<input class="input" id={inputName} bind:value={answer} />
+			<input class="input" type="text" id={inputName} />
+		{:else if question.type === 'table_radio'}
+			<table class="table table-sm">
+				<thead>
+					<tr>
+						{#each question.headers as header}
+							<th scope="col">{header}</th>
+						{/each}
+					</tr>
+				</thead>
+				<tbody>
+					{#each question.rows as row, i}
+						<tr>
+							<th>{row}</th>
+							{#each question.headers as header}
+								<td>
+									<input type="radio" name={inputName + '-' + i} value={header} class="radio" />
+								</td>
+							{/each}
+						</tr>
+					{/each}
+				</tbody>
+			</table>
 		{/if}
 
 		<div class="justify-end card-actions">
@@ -41,8 +62,7 @@
 					step++;
 					question = questions[step - 1];
 					inputName = `question-${step}`;
-					console.log(answer);
-					answer = '';
+					getAnswers();
 				}}>Proximo</button
 			>
 		</div>
