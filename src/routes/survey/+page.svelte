2372f340afaf5ca61614d4286f02110bda09298c
Jahn16 <jpdeoliveiraferreira@gmail.com>
2025-07-03 19:52:29 -0300

    fix: fix styling

--- src/routes/survey/+page.svelte
+++ src/routes/survey/+page.svelte
@@ -15,56 +15,55 @@
 </script>
 
 <!-- eslint-disable svelte/require-each-key -->
-<div></div>
-<div class="card w-96 bg-base-100 card-lg shadow-sm">
-	<div class="card-body">
-		<h2 class="card-title">
-			{question.question}
-		</h2>
-		{#if question.options}
-			{#each question.options as option}
+<div class="container-md">
+	<h2 class="card-title mb-5">
+		{question.question}
+	</h2>
+	{#if question.options}
+		{#each question.options as option}
+			<div class="mt-3">
 				<label>
 					<input type="radio" name={inputName} value={option} class="radio" />
 					{option}
-				</label><br />
-			{/each}
-		{:else if question.type === 'text'}
-			<input class="input" type="text" id={inputName} />
-		{:else if question.type === 'table_radio'}
-			<table class="table table-sm">
-				<thead>
+				</label>
+			</div>
+		{/each}
+	{:else if question.type === 'text'}
+		<input class="input" type="text" id={inputName} />
+	{:else if question.type === 'table_radio'}
+		<table class="table table-sm">
+			<thead>
+				<tr>
+					{#each question.headers as header}
+						<th scope="col">{header}</th>
+					{/each}
+				</tr>
+			</thead>
+			<tbody>
+				{#each question.rows as row, i}
 					<tr>
-						{#each question.headers as header}
-							<th scope="col">{header}</th>
+						<th>{row}</th>
+						{#each question.headers.slice(1) as header}
+							<td>
+								<input type="radio" name={inputName + '-' + i} value={header} class="radio" />
+							</td>
 						{/each}
 					</tr>
-				</thead>
-				<tbody>
-					{#each question.rows as row, i}
-						<tr>
-							<th>{row}</th>
-							{#each question.headers as header}
-								<td>
-									<input type="radio" name={inputName + '-' + i} value={header} class="radio" />
-								</td>
-							{/each}
-						</tr>
-					{/each}
-				</tbody>
-			</table>
-		{/if}
+				{/each}
+			</tbody>
+		</table>
+	{/if}
 
-		<div class="justify-end card-actions">
-			<button
-				class="btn btn-primary"
-				type="submit"
-				on:click={() => {
-					step++;
-					question = questions[step - 1];
-					inputName = `question-${step}`;
-					getAnswers();
-				}}>Proximo</button
-			>
-		</div>
+	<div class="justify-end card-actions">
+		<button
+			class="btn btn-primary"
+			type="submit"
+			on:click={() => {
+				step++;
+				question = questions[step - 1];
+				inputName = `question-${step}`;
+				getAnswers();
+			}}>Proximo</button
+		>
 	</div>
 </div>
