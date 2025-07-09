5f0abb9f9d9e55e07ce797cec42e610273cca9ef
Jahn16 <jpdeoliveiraferreira@gmail.com>
2025-07-09 17:06:56 -0300

    feat: add play form action

---
+++ src/routes/play/+page.server.ts
@@ -0,0 +1,14 @@
+import type { Actions } from './$types';
+import { savePlayData } from '$lib/server/db';
+import { redirect } from '@sveltejs/kit';
+
+export const actions = {
+	default: async ({ request }) => {
+		const formData = await request.formData();
+		const playData = Object.fromEntries(formData.entries()) as { [key: string]: string };
+		console.log('Play data received:', playData);
+
+		const playID = savePlayData(playData);
+		redirect(303, `/survey?play-id=${playID}`);
+	}
+} satisfies Actions;
