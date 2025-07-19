7fc34e0ea3dec058f2a9ffff3a81232940b33bc9
Jahn16 <jpdeoliveiraferreira@gmail.com>
2025-07-19 15:34:31 -0300

    feat: add end page

---
+++ src/routes/end/+page.svelte
@@ -0,0 +1,15 @@
+<div class="container mx-5">
+	<h1>Fim da pesquisa!</h1>
+	<br />
+	<p>Obrigado por ter participado</p>
+</div>
+
+<style>
+	h1 {
+		font-size: 3rem;
+		font-weight: bold;
+	}
+	p {
+		font-size: 1.2rem;
+	}
+</style>
--- src/routes/survey/+page.server.ts
+++ src/routes/survey/+page.server.ts
@@ -1,10 +1,12 @@
 import type { Actions } from './$types';
 import { submitSurvey } from '$lib/server/db';
+import { redirect } from '@sveltejs/kit';
 
 export const actions = {
 	default: async ({ request }) => {
 		const data = await request.formData();
 		const anwers = Object.fromEntries(data.entries()) as { [key: string]: string };
 		await submitSurvey(anwers);
+		redirect(303, '/end');
 	}
 } satisfies Actions;
